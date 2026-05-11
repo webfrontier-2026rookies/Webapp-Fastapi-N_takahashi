8e84028c3ef0fb87a2fefcbfed325e9f930a8fa7
webfrontier-niko <n_takahashi@webfrontier.co.jp>
2026-05-11 12:12:32 +0900

    tailwindcss追加と一覧ページの内容と見た目追加

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# --- トップページ (/) 用の道 ---
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse(
