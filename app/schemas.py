ff90aa33eb049a1c7977cc3a191e8df4e48be832
webfrontier-niko <n_takahashi@webfrontier.co.jp>
2026-05-21 11:33:55 +0900

    重複しているコードを削除

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
 from datetime import datetime
+from pydantic import HttpUrl
 
 # データの受け取り用スキーマ
 class TodoBase(BaseModel):
@@ -8,36 +9,33 @@ class TodoBase(BaseModel):
     description: str
     
 class TodoCreate(TodoBase):
-    created_at: Optional[datetime] = None
     due_date: Optional[datetime] = None
     status: Optional[bool] = False
     tag: Optional[str] = None
-    link: Optional[str] = None
+    link: Optional[HttpUrl] = None
+    memo: Optional[str] = None
+
+class TodoUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    due_date: Optional[datetime] = None
+    status: Optional[bool] = None
+    tag: Optional[str] = None
+    link: Optional[HttpUrl] = None
     memo: Optional[str] = None
 
 # DBから取得したデータの返し用スキーマ
 class Todo(TodoBase):
     id: int
-    created_at: Optional[datetime] = None
-    due_date: Optional[datetime] = None
-    status: Optional[bool] = False
-    tag: Optional[str] = None
-    link: Optional[str] = None
+    created_at: datetime
+    due_date: datetime
+    status: bool
+    tag: str
+    link: Optional[HttpUrl] = None
     memo: Optional[str] = None
 
     # SQLAlchemyのモデルをPydanticで使うための設定
     model_config = ConfigDict(from_attributes=True)
-
-# 更新用スキーマ
-class TodoUpdate(TodoBase):
-    title: Optional[str] = None
-    description: Optional[str] = None
-    created_at: Optional[datetime] = None
-    due_date: Optional[datetime] = None
-    status: Optional[bool] = None
-    tag: Optional[str] = None
-    link: Optional[str] = None
-    memo: Optional[str] = None
 
 # タグのスキーマ
 class TagBase(BaseModel):
@@ -45,36 +43,18 @@ class TagBase(BaseModel):
     description: str
 
 class TagCreate(TagBase):
-    title: Optional[str] = None
-    created_at: Optional[datetime] = None
-    description: Optional[str] = None
     usage: Optional[str] = None
 
-class TagUpdate(TagBase):
+class TagUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
-    created_at: Optional[datetime] = None
     usage: Optional[str] = None
 
 # DBから取得したデータの返し用スキーマ
 class Tag(TagBase):
     id: int
-    title: Optional[str] = None
-    description: Optional[str] = None
-    created_at: Optional[datetime] = None
+    created_at: datetime
     usage: Optional[str] = None
-
-    model_config = ConfigDict(from_attributes=True)
-
-    link: Optional[str] = None
-    memo: Optional[str] = None
 
     # SQLAlchemyのモデルをPydanticで使うための設定
     model_config = ConfigDict(from_attributes=True)
-
-# 更新用スキーマ
-class TagUpdate(TagBase):
-    title: Optional[str] = None
-    description: Optional[str] = None
-    created_at: Optional[datetime] = None
-    usage: Optional[str] = None
