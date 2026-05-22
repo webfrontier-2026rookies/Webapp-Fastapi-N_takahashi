cc52599a1bddb37345890982e3cf514ef980f976
webfrontier-niko <n_takahashi@webfrontier.co.jp>
2026-05-22 15:51:31 +0900

    alembic.iniにdburlが直接かかないでうごかせるように調整

--- migration/env.py
+++ migration/env.py
@@ -9,6 +9,12 @@ import app.models
 
 config = context.config
 
+database_url = os.getenv("DATABASE_URL")
+if not database_url:
+    raise RuntimeError("DATABASE_URL environment variable is not set.")
+
+config.set_main_option("sqlalchemy.url", database_url)
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
