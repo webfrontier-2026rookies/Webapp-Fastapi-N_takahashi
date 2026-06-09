3ed77ffab6cdca306a5868982f90e1400eb3a6f3
webfrontier-niko <n_takahashi@webfrontier.co.jp>
2026-06-09 10:20:22 +0900

    テストファイル追加

---
+++ tests/test_todo_02.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+from datetime import datetime
+from fastapi.testclient import TestClient
+from main import app
+from app.models import Todo, TodoTag
+from app.database import get_db
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+client = TestClient(app)
+
+#todoの一覧表示ができるかどうかのテストコード
+def test_todo_list():
+    db = next(get_db())
+    db.query(TodoTag).delete()
+    db.query(Todo).delete()
+    
+    test_todo = Todo(
+        title="一覧表示のテスト用TODO",
+        description="このTODOが画面やAPIから見えれば合格です",
+        due_date=datetime.now(),
+        status=False,
+        username=2525,
+    )
+    db.add(test_todo)
+    db.commit()
+
+    response = client.get("/api/todo")
+
+    assert response.status_code == 200
+
+    db.close()
